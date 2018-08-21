2a6e869c50a1f4d1a22b65ceddb110367512004a
Tommy G <8443884+VmX5@users.noreply.github.com>
2018-08-21 18:02:30 -0400

    Add files via upload

---
+++ Bouncing Ball/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Bouncing_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.world1 = new World();
+            this.button1 = new System.Windows.Forms.Button();
+            this.WorldTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // world1
+            // 
+            this.world1.Location = new System.Drawing.Point(13, 13);
+            this.world1.Name = "world1";
+            this.world1.Size = new System.Drawing.Size(500, 500);
+            this.world1.TabIndex = 0;
+            this.world1.Text = "world1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WorldTimer
+            // 
+            this.WorldTimer.Interval = 12;
+            this.WorldTimer.Tick += new System.EventHandler(this.WorldTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 548);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.world1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private World world1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer WorldTimer;
+    }
+}
+
---
+++ Bouncing Ball/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bouncing_Ball {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }  
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (WorldTimer.Enabled) {
+                WorldTimer.Enabled = false;
+            } else {
+                WorldTimer.Enabled = true;
+            }
+        }
+
+        private void WorldTimer_Tick(object sender, EventArgs e) {
+            world1.Invalidate();
+        }
+    }
+}
+
+public class World : Control
+{
+    private Vector Ball = new Vector(200, 200);
+    private Vector speed = new Vector(2, 5);
+
+    public World()
+    {
+        DoubleBuffered = true;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        Graphics g = e.Graphics;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+        g.Clear(Color.White);
+
+        update();
+        g.FillEllipse(Brushes.Red, new Rectangle((int)Ball.x, (int)Ball.y, 50, 50));
+        g.DrawEllipse(new Pen(Brushes.Black, 2), new Rectangle((int)Ball.x, (int)Ball.y, 50, 50));
+    }
+
+    public void update()
+    {
+        Ball = Ball.add(speed);
+        if ((Ball.x > Width - 50) | (Ball.x < 50))
+            speed.x = speed.x * -1;
+        if ((Ball.y > Height - 50) | (Ball.y < 50))
+            speed.y = speed.y * -1;
+    }
+}
+
+public class Vector
+{
+    public double x;
+    public double y;
+
+    public Vector(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double magnitude()
+    {
+        return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+    }
+
+
+    public Vector add(Vector v1)
+    {
+        return new Vector(x + v1.x, y + v1.y);
+    }
+    public Vector add(Vector v1, Vector v2)
+    {
+        return new Vector(v1.x + v2.x, v1.y + v2.y);
+    }
+}
